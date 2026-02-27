ef4e863b236f8e556087d1e45527a099ef492bf7
gitmarco99 <git.marco99@gmail.com>
2026-02-27 10:22:11 +0100

    ⬆️ ASIR: Subida de nuevos materiales y prácticas

--- ASIR/base datos/MYSQL/PRACTICAS CLASE/EJERICIO 3_MARCOGALLEGO.sql
+++ ASIR/base datos/MYSQL/PRACTICAS CLASE/EJERICIO 3_MARCOGALLEGO.sql
@@ -6,7 +6,21 @@ use EJERCICIO3;
 create table empleados (
 	id_empleado INT unsigned auto_increment,
     DNI VARCHAR(9) NOT NULL,
-    salario DECIMAL(5, 2 ),
+    salario DECIMAL(6, 2 ),
     estado ENUM ('ACTIVO', 'INACTIVO'),
     CONSTRAINT pk_id_empleado PRIMARY KEY (id_empleado)
 );
+
+create table proyectos(
+	id_proyecto INT unsigned auto_increment,
+    nombre varchar(40) unique,
+    fecha_inicio date NOT NULL,
+    fecha_fin date,
+    constraint fk_id_departamento foreign key (id_departamento)
+    REFERENCES compositor (id)
+        ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+create table asignaciones (
+	id_empleado 
+    
